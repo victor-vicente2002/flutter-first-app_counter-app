069c1ca051005ed2b37f29e20b06bb266674f8b8
victor-vicente2002 <victor.casimiro@bandtec.com.br>
2021-06-22 18:25:13 -0300

    Primeiro app em flutter aula 1 da nlw

---
+++ counter_app/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(primarySwatch: Colors.purple),
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 0;
+
+  void increment() {
+    count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Conter app"),
+      ),
+      body: Center(
+          child: Text(
+        "COUNTER\n$count",
+        textAlign: TextAlign.center,
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },
+      ),
+    );
+  }
+}
